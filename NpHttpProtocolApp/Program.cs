e86a9bffc00d27ec39cbfe88d510a305c6b4b84c
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-08-13 21:16:59 +0300

    Добавьте файлы проекта.

---
+++ NpHttpProtocolApp/Program.cs
@@ -0,0 +1,12 @@
+﻿HttpMessageHandler handler = new HttpClientHandler();
+
+Console.WriteLine("Start");
+for(int i = 0; i < 1; i++)
+{
+    using(var client = new HttpClient(handler, false))
+    {
+        using var result = await client.GetAsync("https://yandex.ru");
+        Console.WriteLine(result.ToString());
+    }
+}
+Console.WriteLine("Finish");
